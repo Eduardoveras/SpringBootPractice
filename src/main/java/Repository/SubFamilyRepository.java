1b05c96df780192d5022a85592054345c5b5df80
Djsiclait <djidjellypas92@gmail.com>
2016-10-05 16:38:58 -0400

    Create SubFamilyRepository interface

---
+++ src/main/java/Repository/SubFamilyRepository.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Djidjelly Siclait on 10/5/2016.
+ */
+package Repository;
+
+import Entiy.SubFamily;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SubFamilyRepository extends JpaRepository<SubFamily, String>{
+
+    public SubFamily findBySubFamilyKey(String subFamilyKey);
+
+    @Query("select s from subfamily s where s.family.familyKey = :familyKey")
+    public List<SubFamily> findByFamilyKey(@Param("familyKey") String familyKey);
+}
