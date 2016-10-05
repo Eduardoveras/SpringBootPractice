23345c1dc315c571ac0babb6e51d60c895a512ab
Djsiclait <djidjellypas92@gmail.com>
2016-10-05 15:17:34 -0400

    Create Client entity

---
+++ src/main/java/Entiy/Client.java
@@ -0,0 +1,73 @@
+/**
+ * Created by Djidjelly Siclait on 10/5/2016.
+ */
+package Entiy;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Client implements Serializable{
+    // Attributes
+    @Id
+    private String identificationNumber; // Cedula
+    private String firstName;
+    private String lastName;
+    private String telephone;
+    private String address;
+
+    // Constructor
+    public Client(){
+
+    }
+
+    public Client(String identificationNumber, String firstName, String lastName, String telephone, String address){
+        this.setIdentificationNumber(identificationNumber);
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setTelephone(telephone);
+        this.setAddress(address);
+    }
+
+    // Getters and Setters
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
