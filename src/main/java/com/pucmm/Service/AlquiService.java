4948b36e9f114b52f0c2debadf23cb72e5781e20
Djsiclait <djidjellypas92@gmail.com>
2016-10-05 22:31:58 -0400

    Create AlquiService class

---
+++ src/main/java/com/pucmm/Service/AlquiService.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Djidjelly Siclait on 10/5/2016.
+ */
+package com.pucmm.Service;
+
+import com.pucmm.Repository.ReceiptRepository;
+import com.pucmm.Repository.RentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlquiService {
+    @Autowired
+    private RentRepository rentRepository;
+    @Autowired
+    private ReceiptRepository receiptRepository;
+
+    
+}
