c420823326f00cb8831fd37efec0fc4447bb816e
Djsiclait <djidjellypas92@gmail.com>
2016-10-13 09:42:53 -0400

    Fix logic error

--- src/main/java/com/pucmm/Controller/loginController.java
+++ src/main/java/com/pucmm/Controller/loginController.java
@@ -4,6 +4,7 @@ import com.pucmm.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,12 +21,18 @@ public class loginController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public ModelAndView getLoginPage(@RequestParam Optional<String> error, @RequestParam("username") String username, @RequestParam("password") String password) {
-
-        if(userService.validateUserAccount(username, password))
-            return new ModelAndView("AlquiService");
+    @RequestMapping("/login")
+    public ModelAndView getLoginPage(@RequestParam Optional<String> error) {
 
         return new ModelAndView("login", "error", error);
+    }
+
+    @PostMapping("/loginUser")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password){
+
+        if(userService.validateUserAccount(username, password))
+            return "redirect:/AlquiService";
+        else
+            return "redirect:/login?error=INVALID%20USER%20ACCOUNT";
     }
 }
